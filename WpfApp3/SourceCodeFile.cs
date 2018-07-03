a35e67b8d8d6e244420e709b0a56eea54bdbc6b7
Sergi Gomez <sergi.gomez@mastercard.com>
2018-07-03 16:43:47 +0100

    Add project files.

---
+++ WpfApp3/SourceCodeFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class SourceCodeFile
+    {
+        public string Name { get; set; }
+
+        public FileInfo FileInfo { get; set; }
+
+        public bool IsFileImplementingPatterns { get; set; }
+    }
+}
+ 
